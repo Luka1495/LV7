8720a55855fe1088fe598e61b70b8a53a82f290f
Luka1495 <62608325+Luka1495@users.noreply.github.com>
2020-05-20 13:13:37 +0200

    Add files via upload

---
+++ LV7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[] { 7, 1, 2, 46, -4, 21, 109.6, -120.09 };
+            NumberSequence sequence = new NumberSequence(arr);        
+
+            Console.WriteLine("Pocetni:\n" + sequence.ToString());
+
+            //BubbleSort bubble = new BubbleSort();
+            //sequence.SetSortStrategy(bubble);
+            //sequence.Sort();
+            //Console.WriteLine("Bubble:\n" + sequence.ToString());
+
+            //SequentialSort sequent = new SequentialSort();
+            //sequence.SetSortStrategy(sequent);
+            //sequence.Sort();
+            //Console.WriteLine("Sekvencijalno:\n" + sequence.ToString());
+
+            CombSort comb = new CombSort();
+            sequence.SetSortStrategy(comb);
+            sequence.Sort();
+            Console.WriteLine("Comb sort:\n" + sequence.ToString());
+
+            LinearSearch linearSearch = new LinearSearch();
+            sequence.SetSearchStrategy(linearSearch);
+            Console.WriteLine(sequence.Search(7));
+        }
+    }
+}
